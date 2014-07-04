1eeefec68fe42d2ef994aa07d233558d1117163c
Alexey Belkevich <belkevich.alexey@gmail.com>
2014-07-04 18:56:36 +0300

    Fixed 'pod install' command in travis configuration file Removed 'sharedInstance' method

--- Categories/OCFuntime+Multiton.m
+++ Categories/OCFuntime+Multiton.m
@@ -7,14 +7,7 @@
 //
 
 #import "OCFuntime+Multiton.h"
-#import "ABMultiton.h"
 
 @implementation OCFuntime (Multiton)
-
-+ (instancetype)sharedInstance
-{
-    return [ABMultiton sharedInstanceOfClass:self.class];
-}
-
 
 @end
